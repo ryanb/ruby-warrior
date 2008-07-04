bf2c998250844c791bd9e90d8ba66199407db63f
Ryan Bates <ryan@railscasts.com>
2008-07-04 14:14:36 -0700

    position can now move relatively

--- lib/ruby_warrior/position.rb
+++ lib/ruby_warrior/position.rb
@@ -18,5 +18,26 @@ module RubyWarrior
       @direction_index -= 4 while @direction_index > 3
       @direction_index += 4 while @direction_index < 0
     end
+    
+    def get_relative(forward, right = 0)
+      @floor.get(*translate_offset(forward, right))
+    end
+    
+    def move(forward, right = 0)
+      obj = @floor.clear(@x, @y)
+      @x, @y = *translate_offset(forward, right)
+      @floor.set(obj, @x, @y)
+    end
+    
+    private
+    
+    def translate_offset(forward, right)
+      case direction
+      when :north then [@x + right, @y - forward]
+      when :east then [@x + forward, @y + right]
+      when :south then [@x - right, @y + forward]
+      when :west then [@x - forward, @y - right]
+      end
+    end
   end
 end
