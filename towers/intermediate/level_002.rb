bbc0c82d08df60dc794d5d534cb54e1aaa762208
Ryan Bates <ryan@railscasts.com>
2008-09-12 22:02:57 -0700

    making intermediate lvl 2 a little more difficult

--- towers/intermediate/level_002.rb
+++ towers/intermediate/level_002.rb
@@ -1,18 +1,18 @@
-#  -----
-# | @ s |
-# |s  S>|
-#  -----
+#  ----
+# |@ s |
+# | sS>|
+#  ----
 
 description "Another large room, but with several enemies blocking your way to the stairs."
 tip "Just like walking, you can attack! and feel in multiple directions (:forward, :left, :right, :backward)."
 
 time_bonus 40
-size 5, 2
-stairs 4, 1
+size 4, 2
+stairs 3, 1
 
 warrior 1, 0, :east do |u|
   u.add_abilities :attack!, :health, :rest!
 end
-unit :sludge, 3, 0, :west
-unit :thick_sludge, 3, 1, :west
-unit :sludge, 0, 1, :north
+unit :sludge, 2, 0, :west
+unit :thick_sludge, 2, 1, :west
+unit :sludge, 1, 1, :north
