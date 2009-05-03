eaa65ecd7b34946f4fd7bd994526b6ec5f0e5e3d
Ryan Bates <ryan@railscasts.com>
2009-05-03 15:29:22 -0700

    improving clue for beginner tower level 6

--- towers/beginner/level_006.rb
+++ towers/beginner/level_006.rb
@@ -4,7 +4,7 @@
 
 description "The wall behind you feels a bit further away in this room. And you hear more cries for help."
 tip "You can walk backward by passing ':backward' as an argument to walk!. Same goes for feel, rescue! and attack!."
-clue "Consider walking :backward to start off, and then switch to :forward when warrior.feel(:backward).wall? returns true."
+clue "Walk backward if you are taking damage from afar and do not have enough health to attack. You may also want to consider walking backward until warrior.feel(:backward).wall?."
 
 time_bonus 55
 size 8, 1
