ad30bc53078c21ceeda349df57b2779be38d45ce
Ryan Bates <ryan@railscasts.com>
2009-05-03 21:54:35 -0700

    adding level 9 to beginning tower

---
+++ towers/beginner/level_009.rb
@@ -0,0 +1,19 @@
+#  -----------
+# |>Ca  @ S wC|
+#  -----------
+
+description "Time to hone your skills and apply all of the abilities that you have learned."
+tip "Watch your back."
+clue "Don't just keep shooting the bow while you are being attacked from behind."
+
+time_bonus 40
+size 11, 1
+stairs 0, 0
+
+warrior 5, 0, :east
+
+unit :captive, 1, 0, :east
+unit :archer, 2, 0, :east
+unit :thick_sludge, 7, 0, :west
+unit :wizard, 9, 0, :west
+unit :captive, 10, 0, :west
