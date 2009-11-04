8b1860fd68c7a9d80d80e936fb3d1d1008237567
Ryan Bates <ryan@railscasts.com>
2009-11-04 10:10:49 -0800

    adding --help option - closes #14

--- lib/ruby_warrior/runner.rb
+++ lib/ruby_warrior/runner.rb
@@ -21,8 +21,10 @@ module RubyWarrior
     
     def parse_options
       options = OptionParser.new 
+      options.banner = "Usage: rubywarrior [options]"
       options.on('-d', '--directory DIR', 'Run under given directory') { |dir| RubyWarrior::Config.path_prefix = dir }
       options.on('-t', '--time SECONDS', 'Delay each turn by seconds') { |seconds| RubyWarrior::UI.delay = seconds.to_f }
+      options.on('-h', '--help', 'Show this message') { puts(options); exit }
       options.parse!(@arguments)
     end
   end
