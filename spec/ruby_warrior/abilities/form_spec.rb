8476928c5c0b3099e7ac5aefa35ce0d59c7a225d
Jim Kirkbride <james.kirkbride@gmail.com>
2011-12-31 14:24:18 -0500

    Updated .gitignore.  Updated spec file require statements.  RSpec automatically includes ./spec in the path.

--- spec/ruby_warrior/abilities/form_spec.rb
+++ spec/ruby_warrior/abilities/form_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
+require 'spec_helper'
 
 describe RubyWarrior::Abilities::Form do
   before(:each) do
--- spec/ruby_warrior/position_spec.rb
+++ spec/ruby_warrior/position_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe RubyWarrior::Position do
   before(:each) do
