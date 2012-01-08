23d468a71e7e7d6e968ea093beed74a631127127
Alan Malloy <alan@malloys.org>
2012-01-07 19:48:02 -0800

    Slightly more forgiving beginner score for level 6.

--- towers/beginner/level_006.rb
+++ towers/beginner/level_006.rb
@@ -7,7 +7,7 @@ tip "You can walk backward by passing ':backward' as an argument to walk!. Same 
 clue "Walk backward if you are taking damage from afar and do not have enough health to attack. You may also want to consider walking backward until warrior.feel(:backward).wall?."
 
 time_bonus 55
-ace_score 110
+ace_score 105
 size 8, 1
 stairs 7, 0
 
