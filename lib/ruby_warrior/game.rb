bea632a081d085c3ab62e325f0b970813aba71b8
Rusty Bailey <rustylbailey@gmail.com>
2013-03-06 22:27:31 -0500

    Fix tower path wildcard

--- lib/ruby_warrior/game.rb
+++ lib/ruby_warrior/game.rb
@@ -156,7 +156,7 @@ module RubyWarrior
     end
     
     def tower_paths
-      Dir[File.expand_path('../../../towers/*', __FILE__)]
+      Dir[File.expand_path('../../../towers', __FILE__) + '/*']
     end
     
     
