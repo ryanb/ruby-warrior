ef93a1b0296a19b77e5020b2a8857865f3372006
smlance <stefan@lances.net>
2013-07-15 09:48:33 -0400

    Moved the reset capability to its own method and added a prompt to it.

--- lib/ruby_warrior/runner.rb
+++ lib/ruby_warrior/runner.rb
@@ -17,29 +17,52 @@ module RubyWarrior
       parse_options
       @game.start
     end
-    
+
+    def reset
+      if ! File.directory?('rubywarrior/')
+        # If the directory doesn't exist, we can't reset the progress.
+        print "No progress to reset.\n" 
+      else
+        # Prompt the user to avoid accidental an reset.
+        print "Are you sure you want to reset your progress? [yn]\n"
+        input = gets.chomp
+
+        if input == 'y'
+          FileUtils.rm_rf('rubywarrior/')
+          print "Progress reset.\n" 
+        elsif input == 'n'
+          print "Progress not reset.\n"
+        else
+          print "Not a valid option (y or n). Please try again.\n"
+          reset
+        end
+
+      end
+      exit
+    end
+
     private
     
     def parse_options
       options = OptionParser.new 
       options.banner = "Usage: rubywarrior [options]"
 
-      options.on('-d', '--directory DIR', "Run under given directory")
+      options.on('-d', '--directory DIR', "Run under given directory") \
       { |dir| Config.path_prefix = dir }
 
-      options.on('-l', '--level LEVEL',   "Practice level on epic")
+      options.on('-l', '--level LEVEL',   "Practice level on epic") \
       { |level| Config.practice_level = level.to_i }
 
-      options.on('-r', '--reset',         "Reset all progress")
-      { FileUtils.rm_rf('rubywarrior/') }
+      options.on('-r', '--reset',         "Reset all progress") \
+      { reset }
 
-      options.on('-s', '--skip',          "Skip user input")
+      options.on('-s', '--skip',          "Skip user input") \
       { Config.skip_input = true }
 
-      options.on('-t', '--time SECONDS',  "Delay each turn by seconds")
+      options.on('-t', '--time SECONDS',  "Delay each turn by seconds") \
       { |seconds| Config.delay = seconds.to_f }
 
-      options.on('-h', '--help',          "Show this message")
+      options.on('-h', '--help',          "Show this message") \
       { puts(options); exit }
 
       options.parse!(@arguments)
